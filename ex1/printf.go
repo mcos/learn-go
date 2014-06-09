77ac12c01d428e9bb81278e40a3a0feaf8654c7d
Mark Costello <mark@markcostello.net>
2014-06-09 10:16:20 -0400

    Finished ex1

---
+++ ex1/printf.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("This is a string: %s\n", "I am a string")
+	fmt.Printf("This is an integer: %d\n", 5)
+	fmt.Printf("This is a floatL %.2f\n", 5.343)
+}
